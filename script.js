172133f816d2281da94a43ad8278a7fd702253bf
chokhelimariami <chokhelimariami618@gmail.com>
2024-10-31 11:54:59 +0100

    js task 1

---
+++ script.js
@@ -0,0 +1,14 @@
+let name1 = "Chloe";
+let firstname = "mtchedlishvili";
+let age = 2;
+let email = " chokhelimariami618@gmail.com";
+let number = 3276146354;
+let isadult= false;
+let isminor=true;
+
+console.log("my daugter's name is",name1, "my daughters firstname is",firstname," her age is",age, "this is my email",email, "this is my number",number,);
+ console.log(typeof isadult);
+ console.log(typeof number);
+ console.log("%c 3276146354 " ,"color:red ");
+console.log("%c false", "color:yellow");
+console.log("%c true","color:yellow");
